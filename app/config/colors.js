107cb41f24adbe63a1687cb1be556a21bf809dfd
Nderim Krasniqi <Nderim.k1@gmail.com>
2021-06-06 08:43:28 +0200

    Initial commit added added screen, text, icon, styles and color template

---
+++ app/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: '#618C8C',
+  secondary: '#ABD9BF',
+  black: '#000',
+  white: '#fff',
+  dark: '#0c0c0c',
+  medium: '#6e6969',
+  light: '#f8f4f4',
+  danger: '#ff5252',
+  error: '#ff9494',
+};
